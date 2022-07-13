aab2baa250297f12987bfc0693ae05b39279a11b
Krisez <krisez@foxmail.com>
2022-07-13 17:29:01 +0800

    init project

---
+++ app/src/main/java/cn/krisez/study/av/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.krisez.study.av
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
