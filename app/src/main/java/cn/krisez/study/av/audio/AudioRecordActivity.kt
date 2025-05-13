4253b3cc5ebf927a7d8ae220dd87eedf6cfde9b6
zhouchaoxing <zhouchaoxing@cmiot.chinamobile.com>
2025-05-13 16:16:23 +0800

    回放声音

--- app/src/main/java/cn/krisez/study/av/audio/AudioRecordActivity.kt
+++ app/src/main/java/cn/krisez/study/av/audio/AudioRecordActivity.kt
@@ -23,6 +23,7 @@ import cn.krisez.study.av.util.PcmToWavUtil
 import kotlinx.coroutines.*
 import java.io.*
 
+
 /**
  * @author krisez
  * audio录制，[AudioRecord]和[MediaRecorder]
@@ -42,6 +43,7 @@ class AudioRecordActivity : AppCompatActivity(), OnClickListener {
         binding.tvMediaPlayer.setOnClickListener(this)
         binding.tvAudioRecord.setOnClickListener(this)
         binding.tvAudioTrack.setOnClickListener(this)
+        binding.tvAudioRecordPlay.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -63,29 +65,166 @@ class AudioRecordActivity : AppCompatActivity(), OnClickListener {
                 binding.tvAudioRecord.isEnabled = false
                 binding.tvMediaPlayer.isEnabled = false
                 binding.tvAudioTrack.isEnabled = false
+                binding.tvAudioRecordPlay.isEnabled = false
                 clickMediaRecord()
             }
+
             binding.tvMediaPlayer -> {
                 binding.tvMediaRecord.isEnabled = false
                 binding.tvAudioRecord.isEnabled = false
                 binding.tvMediaPlayer.isEnabled = true
                 binding.tvAudioTrack.isEnabled = false
+                binding.tvAudioRecordPlay.isEnabled = false
                 clickMediaPlayer()
             }
+
             binding.tvAudioRecord -> {
                 binding.tvMediaRecord.isEnabled = false
                 binding.tvAudioRecord.isEnabled = true
                 binding.tvMediaPlayer.isEnabled = false
                 binding.tvAudioTrack.isEnabled = false
+                binding.tvAudioRecordPlay.isEnabled = false
                 clickAudioRecord()
             }
+
             binding.tvAudioTrack -> {
                 binding.tvMediaRecord.isEnabled = false
                 binding.tvAudioRecord.isEnabled = false
                 binding.tvMediaPlayer.isEnabled = false
                 binding.tvAudioTrack.isEnabled = true
+                binding.tvAudioRecordPlay.isEnabled = false
                 clickAudioTrack()
             }
+
+            binding.tvAudioRecordPlay -> {
+                if (!checkHeadsetEnable()) {
+                    Toast.makeText(this, "请插入耳机或者使用蓝牙设备", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                binding.tvMediaRecord.isEnabled = false
+                binding.tvAudioRecord.isEnabled = false
+                binding.tvMediaPlayer.isEnabled = false
+                binding.tvAudioTrack.isEnabled = false
+                binding.tvAudioRecordPlay.isEnabled = true
+               /* CoroutineScope(Dispatchers.Default).launch {
+                    while (true) {
+                        Log.d("Krisez", "onClick: 检测是否拔耳机")
+                        if (!checkHeadsetEnable()) {
+                            clickAudioRecord()
+                            break
+                        }
+                        delay(1000)
+                    }
+                }*/
+                clickRecordAndPlay()
+            }
+        }
+    }
+
+    private fun checkHeadsetEnable(): Boolean {
+        val audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        var isAudioNotLocale = false
+        for (device in audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS)) {
+            binding.tvAudioRecordPlay.text = "${binding.tvAudioRecordPlay.text} + ${device.type}\n"
+            when (device.type) {
+                AudioDeviceInfo.TYPE_USB_HEADSET,
+                AudioDeviceInfo.TYPE_WIRED_HEADSET,
+                AudioDeviceInfo.TYPE_WIRED_HEADPHONES,
+                AudioDeviceInfo.TYPE_BLE_HEADSET,
+                AudioDeviceInfo.TYPE_BLE_SPEAKER,
+                AudioDeviceInfo.TYPE_BLUETOOTH_A2DP,
+                AudioDeviceInfo.TYPE_BLUETOOTH_SCO -> {
+                    // 插入耳机
+                    isAudioNotLocale = true
+                }
+            }
+        }
+        return isAudioNotLocale
+    }
+
+    /**
+     *
+     */
+
+    private fun clickRecordAndPlay() {
+        if (isRecord) {
+            job?.cancel(null)
+            ar?.stop()
+            ar?.release()
+            ar = null
+            at?.stop()
+            at?.release()
+            at = null
+            binding.tvAudioRecordPlay.text = "边录边播，输出不能用手机自带扬声器，需要连接其他设备"
+            resetAllBtn()
+            isRecord = false
+            isPlay = false
+            return
+        }
+
+        minBuffer = AudioRecord.getMinBufferSize(
+            frequency,
+            AudioFormat.CHANNEL_IN_STEREO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+            ar = AudioRecord(
+                AudioSource.MIC,
+                frequency,
+                AudioFormat.CHANNEL_IN_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                minBuffer
+            )
+            val data = ByteArray(minBuffer)
+            ar?.startRecording()
+            isRecord = true
+            val outMinBuffer = AudioTrack.getMinBufferSize(
+                frequency,
+                AudioFormat.CHANNEL_OUT_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT
+            )
+            val aa = AudioAttributes.Builder().setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build()
+            val af = AudioFormat.Builder()
+                .setSampleRate(frequency)
+                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO).build()
+            at = AudioTrack(
+                aa,
+                af,
+                outMinBuffer,
+                AudioTrack.MODE_STREAM,
+                AudioManager.AUDIO_SESSION_ID_GENERATE
+            )
+            at?.play()
+            isPlay = true
+            try {
+                //开启协程，java代码可开启线程
+                job = CoroutineScope(Dispatchers.IO).launch {
+                    withContext(Dispatchers.IO) {
+                        while (isRecord) {
+                            val read = ar?.read(data, 0, minBuffer) ?: -1
+                            if (read > 0) {
+                                try {
+                                    val size = data.size
+                                    if (size > 0) {
+                                        at?.write(data, 0, data.size)
+                                    }
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                }
+                            }
+                        }
+                    }
+                }
+                Log.d("AudioRecordActivity", "clickRecordAndPlay: 开始录音${job?.start()}")
+                binding.tvAudioRecordPlay.text = "正在录音"
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        } else {
+            Toast.makeText(this, "未授权", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -94,6 +233,7 @@ class AudioRecordActivity : AppCompatActivity(), OnClickListener {
         binding.tvAudioRecord.isEnabled = true
         binding.tvMediaPlayer.isEnabled = true
         binding.tvAudioTrack.isEnabled = true
+        binding.tvAudioRecordPlay.isEnabled = true
     }
 
     /**
